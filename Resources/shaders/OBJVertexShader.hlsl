1a53ef5c41dd337c1b7701aaccbdae656f21522f
etigoshun <k021g1375@g.neec.ac.jp>
2022-12-14 14:59:43 +0900

    5_3完了

--- Resources/shaders/OBJVertexShader.hlsl
+++ Resources/shaders/OBJVertexShader.hlsl
@@ -6,36 +6,11 @@ VSOutput main(float4 pos : POSITION, float3 normal : NORMAL, float2 uv : TEXCOOR
 	float4 wnormal = normalize(mul(world, float4(normal, 0)));
 	float4 wpos = mul(world, pos);
 		
-	// ピクセルシェーダーに渡す値
-	VSOutput output; 
+	VSOutput output;	// ピクセルシェーダーに渡す値
 	output.svpos = mul(mul(viewproj, world),pos);
-	
-	//Lamber反射の計算
-	output.color.rgb = dot(lightv, wnormal.xyz) * m_diffuse * lightcolor;
-	output.color.a = m_alpha;
+	output.worldpos = wpos;
+	output.normal = wnormal.xyz;
 	output.uv = uv; 
-
-	//環境反射光
-	float3 ambient = m_ambient;
-
-	//拡散反射光
-	float3 diffuse = dot(lightv, wnormal.xyz) * m_diffuse;
-
-	//光沢度
-	const float shininess = 4.0f;
-
-	//頂点から視点への方向ベクトル
-	float3 eyedir = normalize(cameraPos - wpos.xyz);
-
-	//反射光ベクトル
-	float3 reflect = normalize(-lightv + 2 * dot(lightv, wnormal.xyz) * wnormal.xyz);
-
-	//鏡面反射光
-	float3 specular = pow(saturate(dot(reflect, eyedir)), shininess) * m_specular;
-
-	//全て加算する
-	output.color.rgb = (ambient + diffuse + specular) * lightcolor;
-	output.color.a = m_alpha;
 
 	return output;
 }
